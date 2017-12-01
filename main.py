9af9a962fad880629b2e5d5fcb058bf1bd1f7bce
hamid <hnabati@gmail.com>
2017-12-01 18:26:48 -0500

    first init

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 14:48:17 2017
+
+@author: hp
+"""
+
+from selenium import webdriver
+import time
+path = "D:\Sel_enium\chromedriver.exe"
+
+driver = webdriver.Chrome(path)
+
+time.sleep(1)
+driver.maximize_window()
+driver.get("http://www.google.com")
+
+time.sleep(3)
+
+searchbox = driver.find_element_by_name("q")
+
+searchbox.send_keys("Hamid Nabati")
+searchbox.submit()
+time.sleep(2)
+
+url = driver.current_url
+print (url)
+
+title = driver.title
+print (title)
+
+links = driver.find_elements_by_class_name ('r')
+print (len(links))
+
+print (links[0].text)
+
+
+
+
+driver.close()
+
